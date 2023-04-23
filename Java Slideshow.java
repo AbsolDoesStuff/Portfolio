8ed2f7ac02e7aa2dcb71e8123371011fb89c6f6b
Ryan Canepa <124547083+AbsolDoesStuff@users.noreply.github.com>
2023-04-23 14:46:57 -0700

    Add files via upload

--- Java Slideshow.java
+++ Java Slideshow.java
@@ -1,3 +1,18 @@
+"java.configuration.runtimes": [
+  {
+    "name": "JavaSE-1.8",
+    "path": "/path/to/jdk-8",
+  },
+  {
+    "name": "JavaSE-11",
+    "path": "/path/to/jdk-11",
+  },
+  {
+    "name": "JavaSE-17",
+    "path": "/path/to/jdk-17",
+    "default": true
+  },
+]
 var slideIndex = 1;
 showDivs(slideIndex);
 
